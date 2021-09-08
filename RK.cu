e6dca45f8fdda7ec08222c0c07261bad258625eb
Timofey Zabelin <83931815+tzabelin@users.noreply.github.com>
2021-09-09 01:16:21 +0300

    Add files via upload

---
+++ RK.cu
@@ -0,0 +1,45 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+template<typename System, typename precision = double>
+__device__ void RK4(System system, precision *x, precision *y, const precision *dx, const int n, const int step)
+{
+	precision dx2 = (*dx) / 2;
+	precision dx3 = (*dx) / 3;
+	precision dx6 = (*dx) / 6;
+
+	precision *y_tmp = new precision(0.0);
+	precision *x_tmp = new precision(0.0);
+
+	precision *k1 = new precision(0.0);
+	precision *k2 = new precision(0.0);
+	precision *k3 = new precision(0.0);
+	precision *k4 = new precision(0.0);
+
+	system(x, y_tmp, k1, n);
+
+	*y_tmp = *y + dx2 * (*k1);
+	*x_tmp = *x + dx2;
+	system(x_tmp, y_tmp, k2, n);
+
+	*y_tmp = *y + dx2 * (*k2);
+	*x_tmp = *x + dx2;
+	system(x_tmp, y_tmp, k3, n);
+
+	*y_tmp = *y + (*dx) * (*k3);
+	*x_tmp = *x + (*dx);
+	system(x_tmp, y_tmp, k4, n);
+
+	y[n] = precision(0.0);
+	x[n] = precision(0.0);
+	y[n] = (*y) + dx6 * (*k1) + dx3 * (*k2) + dx3 * (*k3) + dx6 * (*k4);
+	x[n] = (*x) + (*dx);
+}
+template<typename System, typename precision = double>
+__global__ void do_step(System system, precision *x, precision *y, const precision *dx, const int n, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		RK4(system, &(x[i*n + threadIdx.x]), &(y[i*n + threadIdx.x]), dx, n, i);
+	}
+}
---
+++ kernel.cu
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "RK.cu"
+
+struct  Simple
+{
+	__device__ void operator()(const double *x, const double *y, double *k, const int &n)
+	{
+		*k = sin(*x);
+	}
+};
+
+int main()
+{
+	const int n = 2;
+	const int size = 500;
+
+	double *x = new double[n*size];
+	x[0] = double(1.0);
+	x[1] = double(4.0);
+	double *y = new double[n*size];
+	y[0] = double(1.0);
+	y[1] = double(16.0);
+	double h(1.0);
+
+	double*x_dev;
+	double*y_dev;
+	double*h_dev;
+
+	cudaSetDevice(0);
+
+	if ((cudaMalloc((void**)&x_dev, sizeof(double)*n*size) ||
+		cudaMalloc((void**)&y_dev, sizeof(double)*n*size) ||
+		cudaMalloc((void**)&h_dev, sizeof(h))) != cudaSuccess)
+	{
+		std::cerr << "CUDA malloc error";
+		exit(-1);
+	}
+
+	if ((cudaMemcpy(x_dev, x, sizeof(double)*n*size, cudaMemcpyHostToDevice) ||
+		cudaMemcpy(y_dev, y, sizeof(double)*n*size, cudaMemcpyHostToDevice) ||
+		cudaMemcpy(h_dev, &h, sizeof(h), cudaMemcpyHostToDevice)) != cudaSuccess)
+	{
+		std::cerr << "CUDA mempcy error";
+		exit(-1);
+	}
+
+	Simple s;
+	do_step << <1, 2 >> > (s, x_dev, y_dev, h_dev, n, size);
+	cudaDeviceSynchronize();
+
+	if ((
+		cudaMemcpy(x, x_dev, sizeof(double)*n*size, cudaMemcpyDeviceToHost) ||
+		cudaMemcpy(y, y_dev, sizeof(double)*n*size, cudaMemcpyDeviceToHost)) != cudaSuccess)
+	{
+		std::cerr << "CUDA mempcy device to host error";
+		exit(-1);
+	}
+
+	cudaFree(x_dev);
+	cudaFree(y_dev);
+	cudaFree(h_dev);
+
+	for (int u = 0; u < n*size; u += 2)
+	{
+		std::cout << x[u];
+		std::cout << "    ";
+		std::cout << y[u];
+		std::cout << "    ";
+		std::cout << y[u + 1];
+		std::cout << "    ";
+		std::cout << std::endl;
+	}
+}
