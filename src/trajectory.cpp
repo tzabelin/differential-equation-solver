bec8620f53f9101530dce57cb4a2addda267b09b
Timofey Zabelin <83931815+tzabelin@users.noreply.github.com>
2024-05-23 15:06:17 +0300

    Update trajectory.cpp

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -7,12 +7,12 @@ void KerrTrajectory::initial_conditions(double r_0, double b_y, double b_z, doub
 	double p[4];
 
 	double theta0 = theta_init;
-	double r0 = 20 * r_0;
+	double r0 = 2 * r_0;
 	double rho2 = pow(r0, 2) + pow(a, 2) * pow(cos(theta0), 2);
 	double rho = sqrt(rho2);
 	double Delta = pow(r0, 2) - 2.0*r0 + pow(a, 2);
-	double by = 20.0*b_y;
-	double bz = 20.0*b_z;
+	double by = 2.0*b_y;
+	double bz = 2.0*b_z;
 
 
 	p[0] = 1.0 / rho * ((pow(r0, 2) + pow(a, 2)) / sqrt(Delta) + a * by*sin(theta0) / sqrt(pow(by, 2) + bz * 82 + pow(r0, 2)));
